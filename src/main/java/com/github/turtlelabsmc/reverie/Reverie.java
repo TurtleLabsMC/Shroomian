d2db409417fa74fe29846b61371b822f523506b5
Safrodev <70856001+Safrodev@users.noreply.github.com>
2021-07-01 09:20:20 -0400

    Updated main registry

--- src/main/java/com/github/turtlelabsmc/reverie/Reverie.java
+++ src/main/java/com/github/turtlelabsmc/reverie/Reverie.java
@@ -1,12 +1,21 @@
 package com.github.turtlelabsmc.reverie;
 
+import com.github.turtlelabsmc.reverie.effects.SporeEffect;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Reverie implements ModInitializer
 {
+
+    public static final StatusEffect SPORE_EFFECT = new SporeEffect();
+
     @Override
     public void onInitialize()
     {
         System.out.println("Hello world, Reverie loading");
+
+        Registry.register(Registry.STATUS_EFFECT, new Identifier("reverie", "spore_effect"), SPORE_EFFECT);
     }
 }
