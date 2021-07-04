c570857f5a59143ca009ba95df759eddac8caa1f
Safrodev <70856001+Safrodev@users.noreply.github.com>
2021-07-04 12:07:10 -0400

    Updated client

--- src/main/java/com/github/turtlelabsmc/reverie/client/ReverieClient.java
+++ src/main/java/com/github/turtlelabsmc/reverie/client/ReverieClient.java
@@ -1,13 +1,19 @@
 package com.github.turtlelabsmc.reverie.client;
 
+import com.github.turtlelabsmc.reverie.registry.ReverieBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
 public class ReverieClient implements ClientModInitializer
 {
+
     @Override
     public void onInitializeClient()
     {
         System.out.println("Reverie client loading");
+
+        BlockRenderLayerMap.INSTANCE.putBlock(ReverieBlocks.LARCIA_BLOCK, RenderLayer.getCutout());
     }
 }
